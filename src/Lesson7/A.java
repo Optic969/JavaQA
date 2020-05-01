baba612226520250008f283cdf437d6ac1a19017
Optic969 <optic969@tut.by>
2020-05-01 15:24:19 +0300

    Added lesson 8 and HW 8

--- src/Lesson7/A.java
+++ src/Lesson7/A.java
@@ -1,11 +1,10 @@
 package Lesson7;
 
-import Lesson6.Employee;
 
 public class A {
     public static void main(String[] args) {
-        //B object = new B();
-        //Lesson6.Employee emp = new Lesson6.Employee("Ivanov",21);
+        B object = new B();
+        Lesson6.Employee emp = new Lesson6.Employee("Ivanov",21);
         System.out.println("Privet vsem!!!");
     }
 }
